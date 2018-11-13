27485ac477c8b904aeefdf3ab60e6c384b5e1d44
chaomy <chaomy@umich.edu>
2018-11-13 14:45:40 -0500

    w

--- CUDA/hw5_gpu.cu
+++ CUDA/hw5_gpu.cu
@@ -27,49 +27,49 @@ __global__ void update(double *A, double *B, int N) {
   }
 }
 
-__global__ void reduceSmemDyn(double *g_idata, double *g_odata,
-                              unsigned int n) {
-  extern __shared__ double smem[];
+// __global__ void reduceSmemDyn(double *g_idata, double *g_odata,
+//                               unsigned int n) {
+//   extern __shared__ double smem[];
 
-  // set thread ID
-  unsigned int tid = threadIdx.x;
-  int *idata = g_idata + blockIdx.x * blockDim.x;
+//   // set thread ID
+//   unsigned int tid = threadIdx.x;
+//   int *idata = g_idata + blockIdx.x * blockDim.x;
 
-  // set to smem by each threads
-  smem[tid] = idata[tid];
-  __syncthreads();
+//   // set to smem by each threads
+//   smem[tid] = idata[tid];
+//   __syncthreads();
 
-  // in-place reduction in global memory
-  if (blockDim.x >= 1024 && tid < 512) smem[tid] += smem[tid + 512];
+//   // in-place reduction in global memory
+//   if (blockDim.x >= 1024 && tid < 512) smem[tid] += smem[tid + 512];
 
-  __syncthreads();
+//   __syncthreads();
 
-  if (blockDim.x >= 512 && tid < 256) smem[tid] += smem[tid + 256];
+//   if (blockDim.x >= 512 && tid < 256) smem[tid] += smem[tid + 256];
 
-  __syncthreads();
+//   __syncthreads();
 
-  if (blockDim.x >= 256 && tid < 128) smem[tid] += smem[tid + 128];
+//   if (blockDim.x >= 256 && tid < 128) smem[tid] += smem[tid + 128];
 
-  __syncthreads();
+//   __syncthreads();
 
-  if (blockDim.x >= 128 && tid < 64) smem[tid] += smem[tid + 64];
+//   if (blockDim.x >= 128 && tid < 64) smem[tid] += smem[tid + 64];
 
-  __syncthreads();
+//   __syncthreads();
 
-  // unrolling warp
-  if (tid < 32) {
-    volatile double *vsmem = smem;
-    vsmem[tid] += vsmem[tid + 32];
-    vsmem[tid] += vsmem[tid + 16];
-    vsmem[tid] += vsmem[tid + 8];
-    vsmem[tid] += vsmem[tid + 4];
-    vsmem[tid] += vsmem[tid + 2];
-    vsmem[tid] += vsmem[tid + 1];
-  }
+//   // unrolling warp
+//   if (tid < 32) {
+//     volatile double *vsmem = smem;
+//     vsmem[tid] += vsmem[tid + 32];
+//     vsmem[tid] += vsmem[tid + 16];
+//     vsmem[tid] += vsmem[tid + 8];
+//     vsmem[tid] += vsmem[tid + 4];
+//     vsmem[tid] += vsmem[tid + 2];
+//     vsmem[tid] += vsmem[tid + 1];
+//   }
 
-  // write result for this block to global mem
-  if (tid == 0) g_odata[blockIdx.x] = smem[0];
-}
+//   // write result for this block to global mem
+//   if (tid == 0) g_odata[blockIdx.x] = smem[0];
+// }
 
 void matrix_update(int N) {
   int NN{N * N};
