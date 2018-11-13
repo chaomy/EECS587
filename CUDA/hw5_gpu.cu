3bdb7d92bdc879e3c7d63fcccc6cb2b1c82ce78a
chaomy <chaomy@umich.edu>
2018-11-13 14:46:30 -0500

    w

--- CUDA/hw5_gpu.cu
+++ CUDA/hw5_gpu.cu
@@ -5,14 +5,14 @@
 using std::cout;
 using std::endl;
 
-__inline__ __device__ void swap(double &a, double &b) {
-  double tmp = a;
+__inline__ __device__ void swap(float &a, float &b) {
+  float tmp = a;
   a = b;
   b = tmp;
 };
 
-__global__ void update(double *A, double *B, int N) {
-  double slot[4];
+__global__ void update(float *A, float *B, int N) {
+  float slot[4];
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < N * N) {
     int ix = idx / N, iy = idx % N;
@@ -27,9 +27,9 @@ __global__ void update(double *A, double *B, int N) {
   }
 }
 
-// __global__ void reduceSmemDyn(double *g_idata, double *g_odata,
+// __global__ void reduceSmemDyn(float *g_idata, float *g_odata,
 //                               unsigned int n) {
-//   extern __shared__ double smem[];
+//   extern __shared__ float smem[];
 
 //   // set thread ID
 //   unsigned int tid = threadIdx.x;
@@ -58,7 +58,7 @@ __global__ void update(double *A, double *B, int N) {
 
 //   // unrolling warp
 //   if (tid < 32) {
-//     volatile double *vsmem = smem;
+//     volatile float *vsmem = smem;
 //     vsmem[tid] += vsmem[tid + 32];
 //     vsmem[tid] += vsmem[tid + 16];
 //     vsmem[tid] += vsmem[tid + 8];
@@ -73,10 +73,10 @@ __global__ void update(double *A, double *B, int N) {
 
 void matrix_update(int N) {
   int NN{N * N};
-  size_t nBytes = NN * sizeof(double);
-  double *A = (double *)malloc(nBytes);
-  double *B = (double *)malloc(nBytes);
-  double res[3] = {0, 0, 0};
+  size_t nBytes = NN * sizeof(float);
+  float *A = (float *)malloc(nBytes);
+  float *B = (float *)malloc(nBytes);
+  float res[3] = {0, 0, 0};
 
   // initialize
   for (int k = NN - 1; k >= 0; --k) {
@@ -84,9 +84,9 @@ void matrix_update(int N) {
     A[k] = (1 + cos(2 * i) + sin(j)), A[k] *= A[k];
   }
 
-  double *d_A, *d_B;
-  cudaMalloc((double **)&d_A, nBytes);
-  cudaMalloc((double **)&d_B, nBytes);
+  float *d_A, *d_B;
+  cudaMalloc((float **)&d_A, nBytes);
+  cudaMalloc((float **)&d_B, nBytes);
 
   cudaMemcpy(d_A, A, nBytes, cudaMemcpyHostToDevice);
   cudaMemcpy(d_B, A, nBytes, cudaMemcpyHostToDevice);
@@ -111,7 +111,7 @@ void matrix_update(int N) {
     cudaMemcpy(d_A, d_B, nBytes, cudaMemcpyDeviceToDevice);
   }
 
-  // reduceSmemDyn<<<grid.x, block.x, BLOCK_X * sizeof(double)>>>(d_A, d_B, NN);
+  // reduceSmemDyn<<<grid.x, block.x, BLOCK_X * sizeof(float)>>>(d_A, d_B, NN);
 
   // stop the timer
   cudaEventRecord(stop);
@@ -121,9 +121,9 @@ void matrix_update(int N) {
   cudaEventElapsedTime(&millisecond, start, stop);
 
   int p1{N / 2 * N + N / 2}, p2{37 * N + 47};
-  cudaMemcpy(&res[0], &d_B[0], sizeof(double), cudaMemcpyDeviceToHost);
-  cudaMemcpy(&res[1], &d_B[p1], sizeof(double), cudaMemcpyDeviceToHost);
-  cudaMemcpy(&res[2], &d_B[p2], sizeof(double), cudaMemcpyDeviceToHost);
+  cudaMemcpy(&res[0], &d_B[0], sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(&res[1], &d_B[p1], sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(&res[2], &d_B[p2], sizeof(float), cudaMemcpyDeviceToHost);
 
   /* end timing */
   cout << " calculation time " << millisecond << " sum = " << res[0]
