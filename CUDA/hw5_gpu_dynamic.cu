4294d27fac05b74eaf47c7852390323c19cb34b6
chaomy <chaomy@umich.edu>
2018-11-13 21:38:35 -0500

    w

--- CUDA/hw5_gpu_dynamic.cu
+++ CUDA/hw5_gpu_dynamic.cu
@@ -92,6 +92,7 @@ void matrix_update(int N, int BLOCK_X = 128) {
   float *B = (float *)malloc(nBytes);
   float res[3] = {0, 0, 0};
 
+  int p1 = N / 2 * N + N / 2, p2 = 37 * N + 47;
   // initialize
   for (int k = NN - 1; k >= 0; --k) {
     int i{k / N}, j{k % N};
