c214d23c85d45b10c8bb56ad4b70ee8a12ebbe85
chaomy <chaomy@umich.edu>
2018-11-13 20:30:38 -0500

    w

--- CUDA/hw5_gpu_dynamic.cu
+++ CUDA/hw5_gpu_dynamic.cu
@@ -30,11 +30,11 @@ __global__ void update(float *A, float *B, int N) {
 // template <unsigned int GRID_X, unsigned int BLOCK_X>
 __global__ void parent(float *A, float *B, int N, int GRID_X, int BLOCK_X) {
   int num_iter = 5;
-  for (int i = 0; i < num_iter; ++i) {
-    printf("I am runing %d", i);
-    update<<<GRID_X, BLOCK_X>>>(A, B, N);
-    update<<<GRID_X, BLOCK_X>>>(A, B, N);
-  }
+  // for (int i = 0; i < num_iter; ++i) {
+  printf("I am runing %d", i);
+  update<<<GRID_X, BLOCK_X>>>(A, B, N);
+  update<<<GRID_X, BLOCK_X>>>(A, B, N);
+  // }
 }
 
 __global__ void reduceSmemDyn(float *A, float *S, int size) {
