d47889e713f3819a1327da329f775b04761f8088
chaomy <chaomy@umich.edu>
2018-12-03 20:15:25 -0500

    add files

---
+++ FINAL_PROJECT/qm_gpu.cu
@@ -0,0 +1,198 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::string;
+using std::unordered_set;
+using std::vector;
+
+inline void split(const string& s, const char* delim, vector<string>& v) {
+  // duplicate original string, return a char pointer and free  memories
+  char* dup = strdup(s.c_str());
+  char* token = strtok(dup, delim);
+  while (token != NULL) {
+    v.push_back(string(token));
+    // the call is treated as a subsequent calls to strtok:
+    // the function continues from where it left in previous invocation
+    token = strtok(NULL, delim);
+  }
+  free(dup);
+}
+
+int in_bit_num, out_bit_num;
+vector<string> in_labels, out_labels;
+vector<string> input, output;
+
+bool comp(int n, string a, string b) {
+  for (int i = 0; i < n; i++) {
+    if (a[i] != b[i] && (a[i] != '2' && b[i] != '2')) return false;
+  }
+  return true;
+}
+
+int checkBITs(int n, string a, string b) {
+  int count = 0, temp;
+  for (int i = 0; i < n; ++i) {
+    if (a[i] != b[i]) {
+      if (++count > 1) return -1;
+      temp = i;
+    }
+  }
+  return count == 1 ? temp : -1;
+}
+
+void readTrueTable(string fname) {
+  ifstream s(fname, std::iostream::in);
+  string line;
+
+  getline(s, line, ' ');
+  getline(s, line);
+  in_bit_num = stoi(line);
+
+  getline(s, line, ' ');
+  getline(s, line);
+  out_bit_num = stoi(line);
+
+  getline(s, line, ' ');
+  getline(s, line);
+  split(line, " ", in_labels);
+
+  getline(s, line, ' ');
+  getline(s, line);
+  split(line, " ", out_labels);
+
+  // read head
+  while (getline(s, line) && (line != ".e")) {
+    vector<string> buff;
+    split(line, " ", buff);
+    input.push_back(buff[0]);
+    output.push_back(buff[1]);
+  }
+}
+
+void prepInput(vector<string>& v) {
+  size_t N{input.size()};
+  v.reserve(N);
+  for (int i = 0; i < N; ++i) {
+    if (output[i][0] == '1' || output[i][0] == '2') {
+      v.push_back(input[i]);
+    }
+  }
+}
+
+inline int convertStr2Num(string s) {
+  int num{0}, base{0};
+  for (int i = s.size() - 1; i >= 0; --i, base *= 3) num += (s[i] - '0') * base;
+  return num;
+}
+
+int main() {
+  readTrueTable("input.pla");
+
+  vector<string> v;
+  vector<string> prime;   // vector<char*> prime;
+  vector<string> result;  // vector<char*> result;
+
+  prepInput(v);
+  vector<string> relative(v);
+
+  cout << "Input " << endl; 
+  std::copy(v.begin(), v.end(), std::ostream_iterator<string>(cout, "\n")); 
+
+  int T{static_cast<int>(1 << in_bit_num)};
+  size_t nBytes = T * sizeof(char); 
+
+  char *A = (int *)malloc(nBytes);
+
+  // parse each string to num
+  for (int i = 0; i < input.size(); ++i) {
+    int in_num = convertStr2Num(input[i]);
+    int out_num = convertStr2Num(output[i]);
+    A[in_num] = out_num;
+  }
+
+  // to be parallelet
+  // for (int i = 0; i < 16; i++) {
+  //   auto it = std::find_if(buckets.begin(), buckets.end(),
+  //                          [](const vector<int>& a) { return a.size(); });
+  //   if (it == buckets.end()) break;
+
+  //   vector<vector<int>> next(17);
+  //   vector<bool> flag(v.size());
+
+  //   // update bucket
+  //   for (int j = 0; j < 16; ++j) {
+  //     for (auto a : buckets[j]) {
+  //       for (auto b : buckets[j + 1]) {
+  //         int res = checkBITs(16, v[a], v[b]);
+  //         if (res != -1) {  // can merge
+  //           flag[a] = 1, flag[b] = 1;
+  //           v[a][res] = '2';
+  //           next[j].push_back(a);
+  //         }
+  //       }
+  //       if (flag[a] == 0) prime.push_back(v[a]);
+  //     }
+  //   }
+  //   buckets = std::move(next);
+  // }
+
+
+  int count;
+  string temp;
+  
+  for (int i = 0; i < relative.size(); i++) {
+    if (relative[i].empty()) continue;
+
+    int count = 0, num = 0;
+    for (int j = 0; j < prime.size(); j++) {
+      if (prime.size() && comp(16, relative[i], prime[j])) {
+        if (++count > 1) break;
+        num = j;
+      }
+    }
+
+    if (count == 1) {  // essential prime implicant
+      result.push_back(prime[num]);
+      for (int j = 0; j < relative.size(); j++) {
+        if (relative[j].size() && comp(16, relative[j], prime[num])) {
+          relative[j] = "";
+        }
+      }
+      prime[num] = "";
+    }
+  }
+
+  int cnt_empty = std::count_if(relative.begin(), relative.end(),
+                                [](string a) { return a.size() == 0; });
+
+  while (cnt_empty < relative.size()) {
+    do {
+      temp = prime.back();
+      prime.pop_back();
+    } while (temp.size() == 0 && prime.size());
+
+    count = 0;
+    for (int i = 0; i < relative.size(); i++) {
+      if (relative[i].size() && comp(16, relative[i], temp)) {
+        relative[i] = "";
+        cnt_empty++;
+        count++;
+      }
+    }
+    if (count > 0) {
+      result.push_back(temp);
+    }
+  }
+
+  cout << "result : " << endl;
+  for (auto item : result) cout << item << endl;
+  return 0;
+}
--- HW5/hw5_gpu.cu
+++ HW5/hw5_gpu.cu
@@ -59,12 +59,6 @@ __global__ void reduceSmemDyn(float *A, float *S, int size) {
     S[blockIdx.x] = sdata[0];  // each block has its sum of threads within
 };
 
-inline int convertStr2Num(string s) {
-  int num{0}, base{0};
-  for (int i = s.size() - 1; i >= 0; --i, base *= 3) num += (s[i] - '0') * base;
-  return num;
-}
-
 void matrix_update(int N, int BLOCK_X = 128) {
   int NN{N * N}; 
   size_t nBytes = NN * sizeof(float);
