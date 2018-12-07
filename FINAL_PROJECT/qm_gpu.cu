a61ead3c1dbf7e0f3fc053dffb853c77876ac9ff
chaomy <chaomy@umich.edu>
2018-12-07 00:36:44 -0500

    return early

--- FINAL_PROJECT/qm_gpu.cu
+++ FINAL_PROJECT/qm_gpu.cu
@@ -76,10 +76,6 @@ __global__ void update(bool* A, int T, int NumThread, int numof2) {
         // only look for pairs when the bit is 0
         if (tmp % 3 == 0) {
           int next = num + exp;
-          // if (next == 981) {
-          //   printf("Ma de, I am %d, next = %d, combined = %d \n", num, next,
-          //          next + exp);
-          // }
           if (A[3 * next]) {
             A[3 * (next + exp)] = true;
             A[3 * num + 1] = true;
@@ -195,6 +191,8 @@ int main() {
       cout << input[i] << " " << in_num << endl;
     }
   }
+
+  return 0;
 
   bool* d_A;
   int* d_primes;
